02e66324844e730e462794c56e81bc96f795acef
facu <facundobrandi20@hotmail.com>
2022-12-13 14:11:10 -0300

    Agregado Identity

--- Tienda_Ropa/Datos/ApplicationDbContext.cs
+++ Tienda_Ropa/Datos/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Tienda_Ropa.Models;
 
 namespace Tienda_Ropa.Datos
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
